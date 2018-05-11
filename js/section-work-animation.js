3b4c174da04ab5ebdc14b24194a756c5bdc21dc9
Chanel Mepschen <chanelmepschen856@hotmail.com>
2018-05-11 14:03:16 +0200

    Merged with homepage-animation

--- js/section-work-animation.js
+++ js/section-work-animation.js
@@ -1,19 +1,16 @@
 /*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
 (function(){
-    var rect,
-        i;
+    var rect;
 
-    var images = document.querySelectorAll('.img_movein'),
-        animation = false;
+    var animation = false;
 
     function init(){
         rect = document.getElementById('section-work').getBoundingClientRect();
 
         if(window.innerWidth > 1000 && window.innerWidth > window.innerHeight){
-            document.querySelector('.section__par.section-work-2').style.display = 'none';
             document.addEventListener('scroll', getScrollDistance);
         } else {
-            document.querySelector('.section__par.section-work-2').style.display = 'block';
+            document.getElementById('section-work').classList.remove('animate');
             document.removeEventListener('scroll', getScrollDistance);
         }
     }
@@ -21,44 +18,8 @@
     function getScrollDistance(){
         if (window.pageYOffset >= rect.top - 80 && animation == false){
             animation = true;
-            document.removeEventListener('scroll', getScrollDistance);
-            startAnimation();
-        } else if (window.pageYOffset >= rect.top - 80){
-            document.querySelector('.section-work-2.section__par').style.display = 'block';
+            document.getElementById('section-work').classList.add('animate');
         }
-    }
-
-    function startAnimation(){
-        var count = 0;
-        document.querySelector('.section-work-1').classList.add('fade-out');
-        document.querySelector('.section__work-button').style.display = 'block';
-        document.querySelector('.section__work-button').style.opacity = '0';
-
-        setTimeout(function(){
-            document.getElementById('section-work').classList.add('move');
-        }, 1000);
-
-        setTimeout(function(){
-            document.querySelector('.section__par.section-work-2').style.display = 'block';
-            document.querySelectorAll('.section-work-2')[0].classList.add('move-in');
-            document.querySelectorAll('.section-work-2')[1].classList.add('move-in');
-        }, 1500);
-        setTimeout(function(){
-            document.querySelector('.section__work-button').style.opacity = '1';
-        }, 3000);
-        setTimeout(function(){
-            images[0].style.right = '-100%';
-        }, 2500);
-        setInterval(function(){
-            count++;
-            images[count].style.right = '0';
-            setTimeout(function(){
-                images[count].style.right = '-100%';
-                if(count == (images.length-1)){
-                    count = -1;
-                }
-            }, 3000);
-        }, 4000);
     }
 
     init();
