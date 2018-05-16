b338a925b2360a0ba558537df4c615b4cdf1860b
Chanel Mepschen <chanelmepschen856@hotmail.com>
2018-05-16 16:35:36 +0200

    small adjustment for the unblurring of the arch on mobile

--- js/games.js
+++ js/games.js
@@ -362,7 +362,7 @@
                         window.removeEventListener('scroll', removeBlur);
                         document.querySelector('#intuition .section__content-wrap').style.opacity = 0;
                         document.querySelector('.arch-intuition').style.filter = 'blur(0px)';
-                        document.querySelector('.arch-intuition').style.transform = 'scale(1) translate(-50%, -50%) !important';
+                        document.querySelector('.arch-intuition.blur').style.transform = 'scale(1) translate(-50%, -50%) !important';
                     }
                 }
              }
